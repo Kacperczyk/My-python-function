cdbf071f7c6d38114c6d5da9054f0513ff4d5147
Kamil Kacperczyk <kacperczyk4@wp.pl>
2021-09-10 10:28:00 +0200

    I added a possibility to save choosen lines in a new csv file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import linecache
+import os
 
 def dictionary_file_reader_choosen_field(path, first_line, last_line):
     with open(path, 'r', newline='', encoding='utf-8') as f:
@@ -28,10 +29,26 @@ def dictionary_file_reader_choosen_field(path, first_line, last_line):
 
         reader_linecacher = [linecache.getline(path, x).strip() for x in range(first_line, last_line + 1)]
         reader2 = csv.DictReader(reader_linecacher, delimiter=';', fieldnames=fieldnames)
-
         for row in reader2:
             for column_name in user_choice_of_field:
                 print(row[column_name], end=' ')
             print('\n', end='')
+
+    path_of_new_file = r'C:\PyCharm\PythonProject\SeleniumPython'
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    request = input('Do you want save choosen lines in file?: ')
+    if request == 'yes' or request == 'y':
+        new_file_name = input('New csv file name: ')
+        with open(os.path.join(path_of_new_file, new_file_name+'.csv'), 'a', newline='', encoding='utf=8')as nf1:
+            nf1_fieldnames = [x for x in user_choice_of_field]
+            nf1_writer = csv.DictWriter(nf1, delimiter=';', fieldnames=nf1_fieldnames)
+            reader_linecacher = [linecache.getline(path, x).strip() for x in range(first_line, last_line + 1)]
+            reader2 = csv.DictReader(reader_linecacher, delimiter=';', fieldnames=fieldnames)
+            for row in reader2:
+                for column_name in user_choice_of_field:
+                    nf1_writer.writerow({column_name: row[column_name]})
+                    print(row[column_name], end=' ')
 
 dictionary_file_reader_choosen_field(r'C:\PyCharm\PythonProject\SeleniumPython\newfile1.csv', 3, 6)
