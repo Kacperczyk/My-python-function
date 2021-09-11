512616170929ec81caa788e3eb37c3c9a0ab9dd6
Kamil Kacperczyk <Kacperczyk4@wp.pl>
2021-09-11 19:22:57 +0200

    I tried to make another dictionary from dictionary one with only keys just in fieldnames

--- main.py
+++ main.py
@@ -42,13 +42,24 @@ def dictionary_file_reader_choosen_field(path, first_line, last_line):
     if request == 'yes' or request == 'y':
         new_file_name = input('New csv file name: ')
         with open(os.path.join(path_of_new_file, new_file_name+'.csv'), 'a', newline='', encoding='utf=8')as nf1:
-            nf1_fieldnames = [x for x in user_choice_of_field]
-            nf1_writer = csv.DictWriter(nf1, delimiter=';', fieldnames=nf1_fieldnames)
+            nf1_writer = csv.DictWriter(nf1, delimiter=';', fieldnames=user_choice_of_field)
             reader_linecacher = [linecache.getline(path, x).strip() for x in range(first_line, last_line + 1)]
             reader2 = csv.DictReader(reader_linecacher, delimiter=';', fieldnames=fieldnames)
-            for row in reader2:
-                for column_name in user_choice_of_field:
-                    nf1_writer.writerow({column_name: row[column_name]})
-                    print(row[column_name], end=' ')
+            xxx = [x for x in reader2]
 
-dictionary_file_reader_choosen_field(r'C:\PyCharm\PythonProject\SeleniumPython\newfile1.csv', 3, 6)
+            for row in xxx:
+                print(row)
+
+
+#dictionary_file_reader_choosen_field(r'C:\PyCharm\PythonProject\SeleniumPython\newfile1.csv', 2, 5)
+
+#mam przerobic ten dictionary na dictionary w zaleznosci od tego ile jest w kluczy do 1 dictionary
+choosen_filed = ['tittle', 'price']
+dict= {'tittle': 'Okazja Alfa Romeo 159 1.9 JTDM 150KM USZKODZONE TURBO', 'price': '6800'}
+dict2 = {}
+
+print(dict[choosen_filed[0]])
+for row in choosen_filed:
+    dict2.update({row: dict[row]})
+
+print(dict2)
